2d1c2ebf27ce8a934e5d04505145a56305878b97
ladurove <ladurove993@gmail.com>
2024-11-08 15:21:07 +0300

    888888888888888888888

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,44 @@
+/* eslint-disable */
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import pluginTSEslint from 'typescript-eslint';
+// import pluginImport from 'eslint-plugin-import';
+import { FlatCompat } from '@eslint/eslintrc';
+import path from "path";
+import {fileURLToPath} from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 /** @type { import("eslint").Linter.Config[] } */
 export default [
+  ...compat.plugins('eslint-plugin-import'),
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {languageOptions: { globals: globals.browser }},
+  ...pluginTSEslint.configs.recommended,
+  {languageOptions: { globals: globals.node }},
   {
+    plugins: {
+      pluginTSEslint,
+    },
     files: ['**/*.{js,mjs,cjs,ts}'],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      'import/extensions': [
+        'error',
+        {
+          js: 'always',
+          ts: 'always',
+        },
+      ],
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
     },
   },
 ];
--- src/brain-progression.ts
+++ src/brain-progression.ts
@@ -1,3 +1,4 @@
 import {runProgressionGame} from "./app/runs/runProgressionGame.js";
+// import {GameController} from "./libs/game/backend/GameController.js";
 
 await runProgressionGame()
