47de7a85ec38a48c61f382709d6654d04ed7e9bb
ladurove <ladurove993@gmail.com>
2024-11-08 13:52:41 +0300

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,16 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type { import("eslint").Linter.Config[] } */
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {languageOptions: { globals: globals.browser }},
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
-    }
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   },
 ];
